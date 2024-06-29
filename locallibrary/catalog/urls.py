6aa7c0aac7dc6d57de82c5a2707010a83e903913
yvon <yvonmanzi@gmail.com>
2024-06-29 07:12:05 +0200

    home page

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -2,4 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # this name parameter is an identifier for this url. we can reference it somewhere else in the app.
+    # pretty cool. <a href="{%url 'index' %}">Home</a>. 
+    # this is called reverse URL mapping. 
+    # Note: We can hard code the link as in <a href="/catalog/">Home</a>),
+    #  but if we change the pattern for our home page, for example, to /catalog/index) the templates will no longer link correctly.
+    #  Using a reversed URL mapping is more robust.
+    path('', views.index, name='index'),
 ]
